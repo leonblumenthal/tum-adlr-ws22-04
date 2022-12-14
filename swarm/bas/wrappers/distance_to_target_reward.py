4d91fc9823c828ac91accf781a7020dd905d13a3
Leon Blumenthal <dev.blumenthal.leon@gmail.com>
2022-12-14 19:46:59 +0100

    Modify distance to target reward wrapper

--- swarm/bas/wrappers/distance_to_target_reward.py
+++ swarm/bas/wrappers/distance_to_target_reward.py
@@ -3,7 +3,7 @@ import numpy as np
 
 
 class DistanceToTargetRewardWrapper(gym.RewardWrapper):
-    """BAS wrapper to compute reward as negative distance between agent and target position."""
+    """BAS wrapper to compute reward as 1 - normalized distance between agent and target position."""
 
     def __init__(self, env: gym.Env, position: np.ndarray):
         """Initialize wrapper and set the reward range.
@@ -15,11 +15,13 @@ class DistanceToTargetRewardWrapper(gym.RewardWrapper):
         super().__init__(env)
 
         self._position = position
+        # TODO: Compute actual max distance possible based on target position.
+        self._max_possible_distance = np.linalg.norm(self.blueprint.world_size)
 
-        # TODO: Compute actual max distance possible.
-        self._reward_range = (-np.linalg.norm(self.blueprint.world_size), 0)
+        self._reward_range = (0, 1)
 
     def reward(self, _):
-        """Compute reward as negative distance between agent and target position."""
-
-        return -np.linalg.norm(self.agent.position - self._position)
+        """Compute reward as 1 - normalized distance between agent and target position."""
+        distance = np.linalg.norm(self.agent.position - self._position)
+        normalized_distance = distance / self._max_possible_distance
+        return 1 - normalized_distance
