c280cba4794ad02a352c677f08744bd0f8754393
Jakob Taube <jakob.taube1q2@gmail.com>
2023-01-19 11:01:20 +0100

    add section+velocity observation renderer

--- swarm/bas/render/renderers/observation.py
+++ swarm/bas/render/renderers/observation.py
@@ -58,6 +58,63 @@ class SectionObservationWrapperRenderer(Renderer):
                 1,
             )
 
+class SectionObservationAndVelocityWrapperRenderer(Renderer):
+    """Renderer for the SectionObservationWrapper drawing observations and sections."""
+
+    WRAPPER = wrappers.SectionAndVelocityObservationWrapper
+
+    def render(self, canvas: pygame.Surface):
+        """Draw observations small circles and lines to the agent.
+        Also draw sections with static lines from the agent to the max range.
+        """
+        agent = self.agent
+        swarm = self.swarm
+        num_sections = self._num_sections
+        max_range = self._max_range
+        section_observation = self.cached_observation[:-1]
+        velocity = self.cached_observation[-1]
+
+        for i, pos in enumerate(section_observation):
+            # Draw line to observation.
+            self.line(
+                canvas,
+                Colors.OBSERVATION,
+                agent.position,
+                agent.position + pos * max_range,
+                2,
+            )
+            # Draw observation circles.
+            self.circle(
+                canvas,
+                Colors.OBSERVATION,
+                agent.position + pos * max_range,
+                swarm.radius / 2,
+            )
+
+            # Draw section lines.
+            angle = i * 2 * np.pi / num_sections
+            self.line(
+                canvas,
+                Colors.MISC,
+                agent.position,
+                agent.position + np.array([np.cos(angle), np.sin(angle)]) * max_range,
+                1,
+            )
+
+            # Draw velocity line.
+            self.line(
+                canvas,
+                Colors.AGENT,
+                agent.position,
+                agent.position + velocity * max_range,
+                2,
+            )
+            self.circle(
+                canvas,
+                Colors.AGENT,
+                agent.position + velocity * max_range,
+                0.2,
+            )
 
 class DistanceSectionObservationWrapperRenderer(Renderer):
     """Renderer for the DistanceSectionObservationWrapper drawing observations and sections."""
