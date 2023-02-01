f8bd748122e85a79c3754e2f6b1cf2d6df95a728
Leon Blumenthal <dev.blumenthal.leon@gmail.com>
2023-02-02 00:27:08 +0100

    Improve training

--- experiments/example/experiment_1/train.py
+++ experiments/example/experiment_1/train.py
@@ -8,9 +8,9 @@ from pathlib import Path
 from experiments.example.env import create_env
 from swarm.training.training import train
 
-curriculum = [(50000, create_env)]
+curriculum = [(200000, create_env)]
 
 if __name__ == "__main__":
 
     experiment_path = Path(__file__).relative_to(Path.cwd() / "experiments").parent
-    train(curriculum, experiment_path, num_processes=6)
+    train(curriculum, experiment_path, num_processes=6, video_every_n_steps=50000)
