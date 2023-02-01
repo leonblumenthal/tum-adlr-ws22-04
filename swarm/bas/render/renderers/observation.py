59ff81d7af8f381b86804acee6dc18ca3e685058
Leon Blumenthal <dev.blumenthal.leon@gmail.com>
2023-02-01 17:24:29 +0100

    Implemented renderer for observation container

--- swarm/bas/render/renderers/observation.py
+++ swarm/bas/render/renderers/observation.py
@@ -1,175 +1,182 @@
+import gymnasium as gym
 import numpy as np
 import pygame
 
 from swarm.bas import wrappers
 from swarm.bas.render.constants import Colors
 from swarm.bas.render.renderers.renderer import Renderer
+from swarm.bas.wrappers.observation import components
 
 
-class SectionObservationWrapperRenderer(Renderer):
-    """Renderer for the SectionObservationWrapper drawing observations and sections."""
+class ObservationContainerWrapperRenderer(Renderer):
+    """Renderer for drawing all components used in the `ObservationContainerWrapper`."""
 
-    WRAPPER = wrappers.SectionObservationWrapper
+    WRAPPER = wrappers.ObservationContainerWrapper
 
-    def render(self, canvas: pygame.Surface):
-        """Draw observations small circles and lines to the agent.
-        Also draw sections with static lines from the agent to the max range.
-        """
-        agent = self.agent
-        swarm = self.swarm
-        num_sections = self._num_sections
-        max_range = self._max_range
+    def __init__(self, render_wrapper: "RenderWrapper", instance: gym.Wrapper):
+        super().__init__(render_wrapper, instance)
 
-        for i, pos in enumerate(self.cached_step.observation):
-            # TODO: Handle target direction differently.
-            if i < len(self.cached_step.observation) - 1:
-                # Draw line to observation.
-                self.line(
-                    canvas,
-                    Colors.OBSERVATION,
-                    agent.position,
-                    agent.position + pos * max_range,
-                    2,
-                )
-                # Draw observation circles.
-                self.circle(
-                    canvas,
-                    Colors.OBSERVATION,
-                    agent.position + pos * max_range,
-                    swarm.radius / 2,
-                )
-            else:
-                # Draw line to observation.
-                self.line(
-                    canvas,
-                    Colors.OBSERVATION,
-                    agent.position,
-                    agent.position + pos * max_range * 1.2,
-                    4,
-                )
-
-            # Draw section lines.
-            angle = i * 2 * np.pi / num_sections
-            self.line(
-                canvas,
-                Colors.MISC,
-                agent.position,
-                agent.position + np.array([np.cos(angle), np.sin(angle)]) * max_range,
-                1,
-            )
-
-class SectionAndVelocityObservationWrapperRenderer(Renderer):
-    """Renderer for the SectionObservationWrapper drawing observations and sections."""
-
-    WRAPPER = wrappers.SectionAndVelocityObservationWrapper
+        self._component_renderers = {
+            components.SectionObservationComponent: self.render_section_observation_component,
+            components.SectionDistanceObservationComponent: self.render_section_distance_observation_component,
+            components.SectionVelocityObservationComponent: self.render_section_velocity_observation_component,
+            components.TargetDirectionObservationComponent: self.render_target_direction_observation_component,
+            components.AgentVelocityObservationComponent: self.render_agent_velocity_observation_component,
+        }
 
     def render(self, canvas: pygame.Surface):
-        """Draw observations small circles and lines to the agent.
-        Also draw sections with static lines from the agent to the max range.
-        """
-        agent = self.agent
-        swarm = self.swarm
-        num_sections = self._num_sections
-        max_range = self._max_range
-        section_observation = self.cached_observation[:-1]
-        velocity = self.cached_observation[-1]
+        """Distribute rendering of observation components to component renderers."""
 
-        for i, pos in enumerate(section_observation):
-            # Draw line to observation.
+        for component, observation in zip(
+            self._components, self.cached_step.observation
+        ):
+            component_renderer = self._component_renderers.get(type(component))
+            if component_renderer:
+                component_renderer(canvas, component, observation)
+
+    def render_section_observation_component(
+        self,
+        canvas: pygame.Surface,
+        component: components.SectionObservationComponent,
+        observation: np.ndarray,
+    ):
+        agent_to_env_rotation = self._shared_computer.agent_to_env_rotation()
+        observation = observation @ agent_to_env_rotation.T
+
+        for section_index, section_position in enumerate(observation):
+            # Boid position
             self.line(
                 canvas,
                 Colors.OBSERVATION,
-                agent.position,
-                agent.position + pos * max_range,
-                2,
-            )
-            # Draw observation circles.
-            self.circle(
-                canvas,
-                Colors.OBSERVATION,
-                agent.position + pos * max_range,
-                swarm.radius / 2,
-            )
-
-            # Draw section lines.
-            angle = i * 2 * np.pi / num_sections
-            self.line(
-                canvas,
-                Colors.MISC,
-                agent.position,
-                agent.position + np.array([np.cos(angle), np.sin(angle)]) * max_range,
-                1,
-            )
-
-            # Draw velocity line.
-            self.line(
-                canvas,
-                Colors.AGENT,
-                agent.position,
-                agent.position + velocity * max_range,
+                self.agent.position,
+                self.agent.position + section_position * component._max_range,
                 2,
             )
             self.circle(
                 canvas,
-                Colors.AGENT,
-                agent.position + velocity * max_range,
-                0.2,
+                Colors.OBSERVATION,
+                self.agent.position + section_position * component._max_range,
+                self.swarm.radius / 2,
             )
 
-class DistanceSectionObservationWrapperRenderer(Renderer):
-    """Renderer for the DistanceSectionObservationWrapper drawing observations and sections."""
+            angle = (
+                section_index * 2 * np.pi / component._num_sections + self.agent.angle
+            )
 
-    WRAPPER = wrappers.DistanceSectionObservationWrapper
-
-    def render(self, canvas: pygame.Surface):
-        """Draw observed distances and sections."""
-        agent = self.agent
-        num_sections = self._num_sections
-        max_range = self._max_range
-
-        angle_step = 2 * np.pi / num_sections
-
-        for i, distance in enumerate(self.cached_step.observation):
-            # Draw section lines.
-            angle = i * angle_step
+            # Section borders
             self.line(
                 canvas,
                 Colors.MISC,
-                agent.position,
-                agent.position + np.array([np.cos(angle), np.sin(angle)]) * max_range,
+                self.agent.position,
+                self.agent.position
+                + np.array([np.cos(angle), np.sin(angle)]) * component._max_range,
                 1,
             )
+
+        # Max range.
+        self.circle(canvas, Colors.MISC, self.agent.position, component._max_range, 1)
+
+    def render_section_distance_observation_component(
+        self,
+        canvas: pygame.Surface,
+        component: components.SectionDistanceObservationComponent,
+        observation: np.ndarray,
+    ):
+        for section_index, section_distance in enumerate(observation):
+            angle = (
+                section_index * 2 * np.pi / component._num_sections + self.agent.angle
+            )
+
+            # Distance
             self.line(
                 canvas,
                 Colors.OBSERVATION,
-                agent.position,
-                agent.position
+                self.agent.position,
+                self.agent.position
                 + np.array(
-                    [np.cos(angle + angle_step / 2), np.sin(angle + angle_step / 2)]
+                    [
+                        np.cos(angle + np.pi / component._num_sections),
+                        np.sin(angle + np.pi / component._num_sections),
+                    ]
                 )
-                * distance,
+                * component._max_range
+                * section_distance,
                 2,
             )
 
+            # Section borders
+            self.line(
+                canvas,
+                Colors.MISC,
+                self.agent.position,
+                self.agent.position
+                + np.array([np.cos(angle), np.sin(angle)]) * component._max_range,
+                1,
+            )
 
-class TargetDirectionObservationWrapperRenderer(Renderer):
-    """Renderer for the TargetDirectionObservationWrapper drawing the direction to the target."""
+        # Max range.
+        self.circle(canvas, Colors.MISC, self.agent.position, component._max_range, 1)
 
-    WRAPPER = wrappers.TargetDirectionObservationWrapper
+    def render_section_velocity_observation_component(
+        self,
+        canvas: pygame.Surface,
+        component: components.SectionVelocityObservationComponent,
+        section_velocities: np.ndarray,
+    ):
+        agent_to_env_rotation = self._shared_computer.agent_to_env_rotation()
+        section_velocities = section_velocities @ agent_to_env_rotation.T
 
-    def render(self, canvas: pygame.Surface):
-        """Draw target direction."""
+        section_boid_indices = self._shared_computer.closest_boid_indices_per_section(
+            component._num_sections,
+            component._max_range + component._subtract_radius * self.env.swarm.radius,
+        )
+
+        for velocity, boid_index in zip(section_velocities, section_boid_indices):
+            if boid_index is None:
+                continue
+
+            self.line(
+                canvas,
+                Colors.OBSERVATION,
+                self.swarm.positions[boid_index],
+                self.swarm.positions[boid_index]
+                + velocity * self.agent.max_velocity * 10,
+                2,
+            )
+
+        # Max range.
+        self.circle(canvas, Colors.MISC, self.agent.position, component._max_range, 1)
+
+    def render_target_direction_observation_component(
+        self,
+        canvas: pygame.Surface,
+        component: components.TargetDirectionObservationComponent,
+        observation: np.ndarray,
+    ):
+
+        agent_to_env_rotation = self._shared_computer.agent_to_env_rotation()
+        observation = agent_to_env_rotation @ observation
 
         self.line(
             canvas,
             Colors.OBSERVATION,
             self.agent.position,
-            self.agent.position + self.cached_step.observation * self.agent.radius * 2,
+            self.agent.position + observation * 10,
             4,
         )
 
-
-class TargetDirectionAndSectionObservationWrapperRenderer(
-    SectionObservationWrapperRenderer
-):
-    WRAPPER = wrappers.TargetDirectionAndSectionObservationWrapper
+    def render_agent_velocity_observation_component(
+        self,
+        canvas: pygame.Surface,
+        component: components.AgentVelocityObservationComponent,
+        observation: np.ndarray,
+    ):
+        agent_to_env_rotation = self._shared_computer.agent_to_env_rotation()
+        observation = agent_to_env_rotation @ observation
+        self.line(
+            canvas,
+            Colors.OBSERVATION,
+            self.agent.position,
+            self.agent.position + observation * self.agent.max_velocity * 10,
+            8,
+        )
