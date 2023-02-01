f6749079bc1d8bc384b05c551d1f38c1cb911039
Leon Blumenthal <dev.blumenthal.leon@gmail.com>
2023-02-01 18:17:45 +0100

    Update observation container wrapper renderer

--- swarm/bas/render/renderers/observation.py
+++ swarm/bas/render/renderers/observation.py
@@ -18,8 +18,9 @@ class ObservationContainerWrapperRenderer(Renderer):
 
         self._component_renderers = {
             components.SectionObservationComponent: self.render_section_observation_component,
-            components.SectionDistanceObservationComponent: self.render_section_distance_observation_component,
             components.SectionVelocityObservationComponent: self.render_section_velocity_observation_component,
+            components.SectionDistanceObservationComponent: self.render_section_distance_observation_component,
+            components.SectionVelocityDistanceObservationComponent: self.render_section_velocity_distance_observation_component,
             components.TargetDirectionObservationComponent: self.render_target_direction_observation_component,
             components.AgentVelocityObservationComponent: self.render_agent_velocity_observation_component,
         }
@@ -44,7 +45,6 @@ class ObservationContainerWrapperRenderer(Renderer):
         observation = observation @ agent_to_env_rotation.T
 
         for section_index, section_position in enumerate(observation):
-            # Boid position
             self.line(
                 canvas,
                 Colors.OBSERVATION,
@@ -63,7 +63,6 @@ class ObservationContainerWrapperRenderer(Renderer):
                 section_index * 2 * np.pi / component._num_sections + self.agent.angle
             )
 
-            # Section borders
             self.line(
                 canvas,
                 Colors.MISC,
@@ -73,48 +72,6 @@ class ObservationContainerWrapperRenderer(Renderer):
                 1,
             )
 
-        # Max range.
-        self.circle(canvas, Colors.MISC, self.agent.position, component._max_range, 1)
-
-    def render_section_distance_observation_component(
-        self,
-        canvas: pygame.Surface,
-        component: components.SectionDistanceObservationComponent,
-        observation: np.ndarray,
-    ):
-        for section_index, section_distance in enumerate(observation):
-            angle = (
-                section_index * 2 * np.pi / component._num_sections + self.agent.angle
-            )
-
-            # Distance
-            self.line(
-                canvas,
-                Colors.OBSERVATION,
-                self.agent.position,
-                self.agent.position
-                + np.array(
-                    [
-                        np.cos(angle + np.pi / component._num_sections),
-                        np.sin(angle + np.pi / component._num_sections),
-                    ]
-                )
-                * component._max_range
-                * section_distance,
-                2,
-            )
-
-            # Section borders
-            self.line(
-                canvas,
-                Colors.MISC,
-                self.agent.position,
-                self.agent.position
-                + np.array([np.cos(angle), np.sin(angle)]) * component._max_range,
-                1,
-            )
-
-        # Max range.
         self.circle(canvas, Colors.MISC, self.agent.position, component._max_range, 1)
 
     def render_section_velocity_observation_component(
@@ -144,8 +101,113 @@ class ObservationContainerWrapperRenderer(Renderer):
                 2,
             )
 
-        # Max range.
         self.circle(canvas, Colors.MISC, self.agent.position, component._max_range, 1)
+
+    def render_section_distance_observation_component(
+        self,
+        canvas: pygame.Surface,
+        component: components.SectionDistanceObservationComponent,
+        observation: np.ndarray,
+    ):
+        for section_index, section_distance in enumerate(observation):
+            angle = (
+                section_index * 2 * np.pi / component._num_sections + self.agent.angle
+            )
+
+            self.line(
+                canvas,
+                Colors.OBSERVATION,
+                self.agent.position,
+                self.agent.position
+                + np.array(
+                    [
+                        np.cos(angle + np.pi / component._num_sections),
+                        np.sin(angle + np.pi / component._num_sections),
+                    ]
+                )
+                * component._max_range
+                * section_distance,
+                2,
+            )
+
+            self.line(
+                canvas,
+                Colors.MISC,
+                self.agent.position,
+                self.agent.position
+                + np.array([np.cos(angle), np.sin(angle)]) * component._max_range,
+                1,
+            )
+
+        self.circle(canvas, Colors.MISC, self.agent.position, component._max_range, 1)
+
+    def render_section_velocity_distance_observation_component(
+        self,
+        canvas: pygame.Surface,
+        component: components.SectionVelocityDistanceObservationComponent,
+        observation: np.ndarray,
+    ):
+
+        agent_to_env_rotation = self._shared_computer.agent_to_env_rotation()
+        section_normals = component._section_normals @ agent_to_env_rotation.T
+        section_tangents = component._section_tangents @ agent_to_env_rotation.T
+
+        boid_distances = self._shared_computer.agent_to_boid_distances()
+        section_boid_indices = self._shared_computer.closest_boid_indices_per_section(
+            component._num_sections,
+            component._max_range + component._subtract_radius * self.env.swarm.radius,
+        )
+        section_distances = [
+            (
+                boid_distances[boid_index]
+                - self.env.swarm.radius * component._subtract_radius
+                if boid_index is not None
+                else component._max_range
+            )
+            for boid_index in section_boid_indices
+        ]
+
+        for section_index, (
+            section_distance,
+            (section_normal_velocity, section_tangent_velocity),
+        ) in enumerate(zip(section_distances, observation)):
+            angle = (
+                section_index * 2 * np.pi / component._num_sections + self.agent.angle
+            )
+
+            anchor_position = (
+                self.agent.position
+                + np.array(
+                    [
+                        np.cos(angle + np.pi / component._num_sections),
+                        np.sin(angle + np.pi / component._num_sections),
+                    ]
+                )
+                * section_distance
+            )
+
+            self.line(
+                canvas,
+                Colors.OBSERVATION,
+                anchor_position,
+                anchor_position
+                + section_normals[section_index]
+                * section_normal_velocity
+                * self.agent.max_velocity
+                * 10,
+                4,
+            )
+            self.line(
+                canvas,
+                Colors.OBSERVATION,
+                anchor_position,
+                anchor_position
+                + section_tangents[section_index]
+                * section_tangent_velocity
+                * self.agent.max_velocity
+                * 10,
+                4,
+            )
 
     def render_target_direction_observation_component(
         self,
