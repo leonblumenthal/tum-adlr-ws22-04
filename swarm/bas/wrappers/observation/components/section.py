7ee42b50a732908b5852a606b58d0efd8f29d642
Leon Blumenthal <dev.blumenthal.leon@gmail.com>
2023-02-01 18:17:21 +0100

    Add section velocity distance observation component

--- swarm/bas/wrappers/observation/components/section.py
+++ swarm/bas/wrappers/observation/components/section.py
@@ -1,6 +1,7 @@
+from typing import Generator
+
 import gymnasium as gym
 import numpy as np
-from typing import Generator
 
 from swarm.bas import BASEnv
 from swarm.bas.wrappers.observation.components.component import (
@@ -42,6 +43,7 @@ class SectionObservationComponent(ObservationComponent):
         actual_max_range = self._max_range + self._subtract_radius * env.swarm.radius
 
         differences = shared_computer.agent_to_boid_differences()
+        distances = shared_computer.agent_to_boid_distances()
         boid_indices = shared_computer.closest_boid_indices_per_section(
             self._num_sections, actual_max_range
         )
@@ -51,28 +53,16 @@ class SectionObservationComponent(ObservationComponent):
             if boid_index is None:
                 continue
 
-            observation[section_index] = differences[boid_index] / self._max_range
+            observation[section_index] = differences[boid_index]
+
+            if self._subtract_radius:
+                observation[section_index] *= (
+                    1 - env.swarm.radius / distances[boid_index]
+                )
+
+            observation[section_index] /= self._max_range
 
         return observation
-
-
-class SectionDistanceObservationComponent(SectionObservationComponent):
-    """BAS observation component to create the section distance observation.
-
-    For each radial section/quadrant, compute the normalized
-    distance (- the radius) to the nearest boid respectively.
-    """
-
-    @property
-    def observation_space(self) -> gym.spaces.Box:
-        return gym.spaces.Box(0, 1, (self._num_sections,))
-
-    def compute_observation(
-        self, env: BASEnv, shared_computer: SharedComputer
-    ) -> np.ndarray:
-        observation = super().compute_observation(env, shared_computer)
-
-        return np.linalg.norm(observation, axis=1)
 
 
 class SectionVelocityObservationComponent(ObservationComponent):
@@ -86,7 +76,7 @@ class SectionVelocityObservationComponent(ObservationComponent):
         self,
         num_sections: int,
         max_range: float,
-        relative_to_agent: bool,
+        relative_to_agent: bool = False,
         subtract_radius: bool = True,
     ):
         self._num_sections = num_sections
@@ -126,3 +116,59 @@ class SectionVelocityObservationComponent(ObservationComponent):
             observation[section_index] = boid_velocity / env.agent.max_velocity
 
         return observation
+
+
+class SectionDistanceObservationComponent(SectionObservationComponent):
+    """BAS observation component to create the section distance observation.
+
+    For each radial section/quadrant, compute the normalized
+    distance (- the radius) to the nearest boid respectively.
+    """
+
+    @property
+    def observation_space(self) -> gym.spaces.Box:
+        return gym.spaces.Box(0, 1, (self._num_sections,))
+
+    def compute_observation(
+        self, env: BASEnv, shared_computer: SharedComputer
+    ) -> np.ndarray:
+        positions = super().compute_observation(env, shared_computer)
+
+        return np.linalg.norm(positions, axis=1)
+
+
+class SectionVelocityDistanceObservationComponent(SectionVelocityObservationComponent):
+    """BAS observation component to create the section velocity distance observation.
+
+    For each radial section/quadrant, compute the normalized
+    (relative) velocity of the nearest boid projected into the section frame respectively.
+    """
+
+    def __init__(
+        self,
+        num_sections: int,
+        max_range: float,
+        relative_to_agent: bool = False,
+        subtract_radius: bool = True,
+    ):
+        super().__init__(num_sections, max_range, relative_to_agent, subtract_radius)
+
+        section_angles = (
+            (np.arange(self._num_sections) + 0.5) / self._num_sections * 2 * np.pi
+        )
+        self._section_normals = np.array(
+            [np.cos(section_angles), np.sin(section_angles)]
+        ).T
+        self._section_tangents = np.array(
+            [np.sin(section_angles), -np.cos(section_angles)]
+        ).T
+
+    def compute_observation(
+        self, env: BASEnv, shared_computer: SharedComputer
+    ) -> np.ndarray:
+        velocities = super().compute_observation(env, shared_computer)
+
+        normal_velocities = (velocities * self._section_normals).sum(axis=1)
+        tangent_velocities = (velocities * self._section_tangents).sum(axis=1)
+
+        return np.stack([normal_velocities, tangent_velocities], axis=1)
