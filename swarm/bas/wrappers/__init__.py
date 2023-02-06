180888b8833456c93599d0ec925256331d1e91e3
Leon Blumenthal <dev.blumenthal.leon@gmail.com>
2023-02-06 16:30:42 +0100

    Remove relative action wrapper

--- swarm/bas/wrappers/__init__.py
+++ swarm/bas/wrappers/__init__.py
@@ -4,7 +4,6 @@ from .boid_collision_wrapper import BoidCollisionWrapper
 from .discrete_action import DiscreteActionWrapper
 from .target_reward import TargetRewardWrapper
 from .num_neighbors_reward import NumNeighborsRewardWrapper
-from .relative_action import RelativeActionWrapper
 from .info_add_wrapper import InfoAddWrapper
 from .trajectory_wrapper import TrajectoryWrapper
 from .random_target_wrapper import RandomTargetWrapper
