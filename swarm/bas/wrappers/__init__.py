0db0c2d5a2277486b095c4a5ff63edfe7bc14445
Jakob Taube <jakob.taube1q2@gmail.com>
2023-02-07 19:49:16 +0100

    Import action wrappers

--- swarm/bas/wrappers/__init__.py
+++ swarm/bas/wrappers/__init__.py
@@ -9,3 +9,6 @@ from .trajectory_wrapper import TrajectoryWrapper
 from .random_target_wrapper import RandomTargetWrapper
 from .observation.container_wrapper import ObservationContainerWrapper
 from .observation.flatten_wrapper import FlattenObservationWrapper
+from .action.angular import AngularActionWrapper
+from .action.angular_and_velocity import AngularAndVelocityActionWrapper
+from .action.desired_velocity import DesiredVelocityActionWrapper
