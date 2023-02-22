1eb2eb5906de77dd53b05e92d4f4af635f08309f
Jakob Taube <jakob.taube1q2@gmail.com>
2023-02-22 23:05:08 +0100

    improve argument parsing

--- utils.py
+++ utils.py
@@ -12,11 +12,11 @@ from stable_baselines3 import PPO
 
 
 def parse_model_env_window_scale() -> tuple[PPO, gym.Env, float]:
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(description="Run a trained model in an environment.")
     parser.add_argument("model_path", type=Path)
-    parser.add_argument("env_path", type=Path)
-    parser.add_argument("env_function", type=str)
-    parser.add_argument("env_call_with_args", type=str)
+    parser.add_argument("env_path", type=Path, help="Path of the file in which the env_function is defined.")
+    parser.add_argument("env_function", type=str, help="Name of the env_function.")
+    parser.add_argument("env_call_with_args", type=str, help='env_function call (with arguments if necessary) in quotes, e.g. "()".')
     parser.add_argument("--window_scale", type=float, default=5)
     args = parser.parse_args()
 
