fc7a142ef1638cb34cb922ffee8ead7b91b4ffc6
Leon Blumenthal <dev.blumenthal.leon@gmail.com>
2023-02-28 21:04:19 +0100

    Move evaluation

---
+++ evaluation/random_to_target/8_desired_velocity_fast/medium/eval.py
@@ -0,0 +1,66 @@
+import sys
+
+sys.path.insert(0, ".")
+
+from pathlib import Path
+
+from evaluation.random_to_target.evaluate import evaluate
+from experiments.random_to_target.env import create_env as create_training_env
+from bas.wrappers.observation import components
+from bas import wrappers
+
+
+def create_env(observation_components):
+    return create_training_env(
+        observation_components + [components.AgentVelocityObservationComponent()],
+        action_wrapper_class=wrappers.DesiredVelocityActionWrapper,
+        action_wrapper_kwargs={},
+        target_radius=3,
+        target_reward=3,
+        distance_reward_transform=lambda d: -(d**0.5) / 500,
+        collision_termination=True,
+        collision_reward=-3,
+        following_weight=0.2,
+        world_size=(200, 200),
+        num_boids=100,
+        max_boid_velocity=0.6,
+        time_limit=500,
+    )
+
+
+def section_distance():
+    return create_env(
+        [
+            components.SectionDistanceObservationComponent(
+                num_sections=8, max_range=20
+            ),
+        ],
+    )
+
+
+def section_distance_and_section_velocity_distance():
+    return create_env(
+        [
+            components.SectionDistanceObservationComponent(
+                num_sections=8, max_range=20
+            ),
+            components.SectionVelocityDistanceObservationComponent(
+                num_sections=8, max_range=20
+            ),
+        ],
+    )
+
+
+if __name__ == "__main__":
+    for name in [
+        "section_distance",
+        "section_distance_and_section_velocity_distance",
+    ]:
+        df = evaluate(
+            f"runs/random_to_target/8_desired_velocity_fast/{name}/model_8.zip",
+            eval(name),
+            num_episodes=10000,
+            num_processes=48,
+        )
+        df.to_csv(Path(__file__).parent / f"{name}.csv")
+        print(name)
---
+++ evaluation/random_to_target/8_desired_velocity_small_range/medium/eval.py
@@ -0,0 +1,87 @@
+import sys
+
+sys.path.insert(0, ".")
+
+from pathlib import Path
+
+from evaluation.random_to_target.evaluate import evaluate
+from experiments.random_to_target.env import create_env as create_training_env
+from bas.wrappers.observation import components
+from bas import wrappers
+
+
+def create_env(observation_components):
+    return create_training_env(
+        observation_components + [components.AgentVelocityObservationComponent()],
+        action_wrapper_class=wrappers.DesiredVelocityActionWrapper,
+        action_wrapper_kwargs={},
+        target_radius=3,
+        target_reward=3,
+        distance_reward_transform=lambda d: -(d**0.5) / 500,
+        collision_termination=True,
+        collision_reward=-3,
+        following_weight=0.2,
+        world_size=(200, 200),
+        num_boids=100,
+        max_boid_velocity=0.6,
+        time_limit=500,
+    )
+
+
+def section():
+    return create_env(
+        [
+            components.SectionObservationComponent(num_sections=8, max_range=10),
+        ],
+    )
+
+
+def section_and_velocity():
+    return create_env(
+        [
+            components.SectionObservationComponent(num_sections=8, max_range=10),
+            components.SectionVelocityObservationComponent(
+                num_sections=8, max_range=10
+            ),
+        ],
+    )
+
+
+def section_distance():
+    return create_env(
+        [
+            components.SectionDistanceObservationComponent(
+                num_sections=8, max_range=10
+            ),
+        ],
+    )
+
+
+def section_distance_and_section_velocity_distance():
+    return create_env(
+        [
+            components.SectionDistanceObservationComponent(
+                num_sections=8, max_range=10
+            ),
+            components.SectionVelocityDistanceObservationComponent(
+                num_sections=8, max_range=10
+            ),
+        ],
+    )
+
+
+if __name__ == "__main__":
+    for name in [
+        "section",
+        "section_and_velocity",
+        "section_distance",
+        "section_distance_and_section_velocity_distance",
+    ]:
+        df = evaluate(
+            f"runs/random_to_target/8_desired_velocity_small_range/{name}/model_8.zip",
+            eval(name),
+            num_episodes=10000,
+            num_processes=48,
+        )
+        df.to_csv(Path(__file__).parent / f"{name}.csv")
+        print(name)
